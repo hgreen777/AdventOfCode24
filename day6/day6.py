209610d4e15bb60cc0ab7f9b415238be91cf8202
Harrison <80791359+hgreen777@users.noreply.github.com>
2025-02-14 14:38:59 +0000

    Proggress

--- day6/day6.py
+++ day6/day6.py
@@ -6,7 +6,7 @@ start_time = datetime.now()
 
 board = []
 
-with open('input.txt','r') as file:
+with open('testInput.txt','r') as file:
     for line in file:
         board.append([char for char in line.strip()])
 
@@ -15,6 +15,7 @@ i=0
 j=0
 current_location = [i,j]
 distinct_positions = 0
+loop_obstructions = 0
 
 for i in range(len(board)):
     for j in range(len(board[i])):
@@ -40,20 +41,25 @@ y = current_location[0]
 x = current_location[1]
 
 # Remove the "^" from board to elimitate any error edge cases -> is not needed as it replaced with effect slector.
-board[y][x] = 'X'
+board[y][x] = '^'
 distinct_positions += 1
 
 # Whilsts guard is on board
+# > 550
 while (True):
     try:
-        if board[y][x] == 'X':
+        if board[y][x] == '^' or board[y][x] == '>' or board[y][x] == 'v' or board[y][x] =='<':
             # Not distinct, move to next square.     
             y += type_effect[effect_selector][1][0]
             x += type_effect[effect_selector][1][1]
+
+            # Suggests a loop
+            if board[y][x] == type_effect[effect_selector][0]:
+                loop_obstructions += 1
         elif board[y][x] == '.':
             # Distinct space.
             distinct_positions +=1
-            board[y][x] = "X"
+            board[y][x] = type_effect[effect_selector][0]
             y += type_effect[effect_selector][1][0]
             x += type_effect[effect_selector][1][1]
         elif board[y][x] == '#':
@@ -69,8 +75,19 @@ while (True):
         print(f"Y:{y},X:{x} and {e}")
         break
 
+# Go through again or go back 
+with open('output.txt', 'w') as file:
+    for lines in board:
+        line_str = ""
+        for x in lines:
+            line_str += str(x)
+        line_str += '\n'
+        file.write(line_str)
+    
+#look for perpendicular lines
 
 print(f"Part 1: {distinct_positions}")
+print(f"Part 2: {loop_obstructions}")
 current_location = [y,x]
 print(f"Current Location of guard (where they left board): {current_location}")
     
