9c1c013b920c9eb9805d7088637eb360fd0e9186
Harrison <80791359+hgreen777@users.noreply.github.com>
2025-02-28 15:17:06 +0000

    Update 14part2.py
    I finally completed part 2. final_pos was causing problems because I forgot how Python manages variables and memory.

--- day14/14part2.py
+++ day14/14part2.py
@@ -1,5 +1,6 @@
+
 # Day 14
-# Part 2 : 
+# Part 2 : 6243
 # After reading problem set and not knowing what the "Christmas Tree" looked like to find.
 # Looked at some solutions, found a video which visualised the board to try and find the tree. I will take a similar approach.
 # https://www.youtube.com/watch?v=EVQwlhDHmJM 
@@ -17,7 +18,7 @@ class Guard:
     def __init__(self, position, velocity): # [], []
         self.pos = position
         self.vel = velocity
-        self.final_pos = self.pos
+        self.final_pos = self.pos.copy() # <-- This caused manyyyy problems. I may have forgotted how variables and memory management works in python.
 
 def readFile(file):
     all_guards = []
@@ -41,23 +42,6 @@ def calculatePosition(guards, dt):
         guard.final_pos[0] = (guard.pos[0] + (guard.vel[0] * dt)) % bounds[0]
         guard.final_pos[1] = (guard.pos[1] + (guard.vel[1] * dt)) % bounds[1]
 
-def calculateSafetyFactor(guards):
-    q1,q2,q3,q4 = 0,0,0,0
-
-    x_line = int(bounds[0] / 2)
-    y_line = int(bounds[1] / 2)
-
-    for guard in guards:
-        if   guard.final_pos[0] < x_line and guard.final_pos[1] < y_line:
-            q1 += 1
-        elif guard.final_pos[0] > x_line and guard.final_pos[1] < y_line:
-            q2 += 1
-        elif guard.final_pos[0] < x_line and guard.final_pos[1] > y_line:
-            q3 += 1
-        elif guard.final_pos[0] > x_line and guard.final_pos[1] > y_line:
-            q4 += 1
-
-    return q1 * q2 * q3 * q4
 
 def clear_lines(num_lines):
     # Move the cursor up `num_lines` times and clear the line 
@@ -72,7 +56,6 @@ def generateBoard(guards, dt):
 
     calculatePosition(guards,dt)
     for guard in guards:
-        #board[guard.final_pos[1]][guard.final_pos[0]] = '#'
         row = list(board[guard.final_pos[1]])
         row[guard.final_pos[0]] = '#'
         board[guard.final_pos[1]] = ''.join(row)
@@ -80,25 +63,30 @@ def generateBoard(guards, dt):
     for x in board:
         print(x)
 
+    print(dt)
+
+    for x in board:
+        if len(findall(r"(#){30,}",x)) > 0:
+
+            time.sleep(3)
+            raise KeyboardInterrupt
+
+
 
 def main():
     guards = readFile(FILE)
-    calculatePosition(guards, 100)
-    total = calculateSafetyFactor(guards)
-    print(f"Part 1 total: {total}")
-
+    #5641
     # TODO : While true, display board for next second passed sleep for 0.1, when space is clicked stop and print the current seconds
     # Can then be manually accessed to check 
     try:
-        current_dt = 0
+        current_dt = 1 #2349 #2349 #<8800 4761
         while True:
             generateBoard(guards, current_dt)
-            time.sleep(0.1)
-            clear_lines(bounds[1])
+            clear_lines(bounds[1] + 1)
             current_dt += 1
     
     except KeyboardInterrupt:
-        print(current_dt)
+        return 
 
  
 
